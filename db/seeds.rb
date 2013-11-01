a01cdd66f70f7265e4d763c3c5f1f8843c1ef694
Scott Norvell <scottlnorvell@gmail.com>
2013-11-01 17:48:43 -0400

    made a stupid-great rake task!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,53 +6,53 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'open-uri'
-require 'zlib'
-require 'yajl'
-# require 'pry'
+# require 'open-uri'
+# require 'zlib'
+# require 'yajl'
+# # require 'pry'
 
-ForkDay.delete_all
+# ForkDay.delete_all
 
-yesterday = Time.new.yesterday.strftime('%F')
+# yesterday = Time.new.yesterday.strftime('%F')
 
-fork_day = ForkDay.create date: yesterday 
+# fork_day = ForkDay.create date: yesterday 
 
-repos = {}
-24.times do |hour|
-	# gz = open("http://data.githubarchive.org/#{yesterday}-15.json.gz")
-	# gz = open("http://data.githubarchive.org/2013-10-31-#{hour}.json.gz")
-	gz = File.open("/Users/scottsMac/Documents/ga_wdi/projects/10_week/gh_data_play/2013-10-31-#{hour}.json.gz",'r')
-	js = Zlib::GzipReader.new(gz).read
-	# gz.close
-	# parse event json and work with indiv events
-	Yajl::Parser.parse(js) do |event|
-	  if event['type'] == 'ForkEvent'
-	  	repo = event['repository']
-	  	repo_id = repo['id']
-			if repos[repo_id]
-				repos[repo_id][:forked_today] += 1
-			else
-				# make a dataset of the values!
-				repos[repo_id] = {
-					forked_today: 1, 
-					name: repo['name'], 
-					repo_url: repo['url'], 
-					total_forks: repo['forks'],
-					language: repo['language']
-				}
-			end
-	  end
-	end
+# repos = {}
+# 24.times do |hour|
+# 	# gz = open("http://data.githubarchive.org/#{yesterday}-15.json.gz")
+# 	# gz = open("http://data.githubarchive.org/2013-10-31-#{hour}.json.gz")
+# 	gz = File.open("/Users/scottsMac/Documents/ga_wdi/projects/10_week/gh_data_play/2013-10-31-#{hour}.json.gz",'r')
+# 	js = Zlib::GzipReader.new(gz).read
+# 	# gz.close
+# 	# parse event json and work with indiv events
+# 	Yajl::Parser.parse(js) do |event|
+# 	  if event['type'] == 'ForkEvent'
+# 	  	repo = event['repository']
+# 	  	repo_id = repo['id']
+# 			if repos[repo_id]
+# 				repos[repo_id][:forked_today] += 1
+# 			else
+# 				# make a dataset of the values!
+# 				repos[repo_id] = {
+# 					forked_today: 1, 
+# 					name: repo['name'], 
+# 					repo_url: repo['url'], 
+# 					total_forks: repo['forks'],
+# 					language: repo['language']
+# 				}
+# 			end
+# 	  end
+# 	end
 
-end
+# end
 
-results = repos.sort_by {|k,v| v[:forked_today]}.reverse[0..100]
+# results = repos.sort_by {|k,v| v[:forked_today]}.reverse[0..100]
 
-p results.first
+# p results.first
 
-results.each do |result|
-	repository = result[1]
-	fork_day.repos << Repo.new(repository) 
-end
+# results.each do |result|
+# 	repository = result[1]
+# 	fork_day.repos << Repo.new(repository) 
+# end
 
 # binding.pry
---
+++ lib/tasks/git_data.rake
@@ -0,0 +1,58 @@
+
+
+
+namespace :git_data do
+
+
+	desc "Pull Yesterday's data from Github"
+	task :go => :environment do
+		require 'open-uri'
+		require 'zlib'
+		require 'yajl'
+		
+		puts "gettin some git data!"
+
+		yesterday = Time.new.yesterday.strftime('%F')
+
+		fork_day = ForkDay.create date: yesterday 
+
+		repos = {}
+		24.times do |hour|
+			gz = open("http://data.githubarchive.org/#{yesterday}-#{hour}.json.gz")
+			# gz = open("http://data.githubarchive.org/2013-10-31-#{hour}.json.gz")
+			# gz = File.open("/Users/scottsMac/Documents/ga_wdi/projects/10_week/gh_data_play/2013-10-31-#{hour}.json.gz",'r')
+			js = Zlib::GzipReader.new(gz).read
+			# gz.close
+			# parse event json and work with indiv events
+			Yajl::Parser.parse(js) do |event|
+			  if event['type'] == 'ForkEvent'
+			  	repo = event['repository']
+			  	repo_id = repo['id']
+					if repos[repo_id]
+						repos[repo_id][:forked_today] += 1
+					else
+						# make a dataset of the values!
+						repos[repo_id] = {
+							forked_today: 1, 
+							name: repo['name'], 
+							repo_url: repo['url'], 
+							total_forks: repo['forks'],
+							language: repo['language']
+						}
+					end
+			  end
+			end
+
+		end
+
+		results = repos.sort_by {|k,v| v[:forked_today]}.reverse[0..100]
+
+		p results.first
+
+		results.each do |result|
+			repository = result[1]
+			fork_day.repos << Repo.new(repository) 
+		end 
+
+	end
+end
