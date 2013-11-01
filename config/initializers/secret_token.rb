140abe798233ae1181b12f8d65267c5474a12d26
Camila Cano <camilacanog@gmail.com>
2013-11-01 14:02:13 -0400

    empty app (rails 4, mongodb, backbone, d3)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubGraph::Application.config.secret_key_base = 'c1eab693cfd7660aa7b4ed90836cbcd26c77a6d61166c899674e7c9351662fb1922a8323e7e172bf21b063f5c20773902f29eaaf83a4a293d7769613a5117ca4'
