f09a54f549848127ff34521a195ccd4af3c6a956
Camila Cano <camilacanog@gmail.com>
2013-11-01 14:30:26 -0400

    updated mongoid's version

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -5,10 +5,6 @@
 //= require_tree ./views
 //= require_tree ./routers
 
-
-// This is included at the beginning of the backbone files to ensure we have an app object as each file is loaded and minimize potential errors
-var app = app || {};
-
 $(function() {
 
   // create the app's router from the Router construct
@@ -16,11 +12,5 @@ $(function() {
 
   // start our backbone history
   Backbone.history.start();
-
-  // Shows how you can capture the event when a URL change occurs
-  Backbone.history.on('route', function() {
-    // This is how to retrieve the request URI / fragment
-    var fragment = Backbone.history.getFragment();
-    console.log(fragment);
   })
 });
